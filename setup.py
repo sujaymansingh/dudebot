74f5c2d97c422358122a47138570c9e0cd6d88f6
Sujay Mansingh <sujay@editd.com>
2012-12-24 14:23:47 +0000

    Make this pip-installable.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='dudebot',
+    version='0.0.1',
+    author='Sujay Mansingh',
+    author_email='sujay.mansingh@gmail.com',
+    packages=['dudebot'],
+    scripts=[],
+    url='https://github.com/sujaymansingh/dudebot',
+    license='LICENSE.txt',
+    description='Useful cricket-related stuff.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'jabberbot==0.15',
+        'xmpppy==0.5.0rc1',
+    ],
+)
