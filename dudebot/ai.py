370c49eba30253f259454884441e9921b51719ab
Sujay Mansingh <sujay@editd.com>
2012-12-24 14:06:59 +0000

    Add some decorators to make life easier.

--- dudebot/ai.py
+++ dudebot/ai.py
@@ -7,7 +7,38 @@ class BotAI(object):
         pass
 
     def respond(self, sender_nickname, message):
-        pass
+        return False, ''
+
+
+class message_must_begin_with_prefix(object):
+    """A simple decorator so that a bot AI can ignore all messages that don't
+    begin with the given prefix.
+    That way you can have your dude bot only respond to messages that, for 
+    example, begin with 'dude '.
+    """
+
+    def __init__(self, desired_prefix):
+        self.desired_prefix = desired_prefix
+
+    def __call__(self, func):
+        def wrapped_func(botai, sender_nickname, message, *args, **kwargs):
+            if message.startswith(self.desired_prefix):
+                return func(botai, sender_nickname, message, *args, **kwargs)
+            else:
+                return False, ''
+        return wrapped_func
+
+
+def message_must_begin_with_nickname(func):
+    """A simple decorator so that a bot AI can ignore all messages that don't
+    begin with the bot AI's nickname.
+    """
+    def wrapped_func(botai, sender_nickname, message, *args, **kwargs):
+        if message.startswith(botai.nickname):
+            return func(botai, sender_nickname, message, *args, **kwargs)
+        else:
+            return False, ''
+    return wrapped_func
 
 
 class Echo(BotAI):
