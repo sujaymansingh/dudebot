397ea537f5064484e2f3d6ff70331b97ab8b4388
Sujay Mansingh <sujay@hoju-jr.local>
2012-12-25 22:50:01 +0000

    Bumping version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='dudebot',
-    version='0.0.1',
+    version='0.0.2',
     author='Sujay Mansingh',
     author_email='sujay.mansingh@gmail.com',
     packages=['dudebot'],
