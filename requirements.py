2aeda240da481e679fa4de834be70407ba23d01a
Sujay Mansingh <sujay@editd.com>
2013-09-24 19:47:51 +0100

    Add way to get requirements from setup.py

---
+++ requirements.py
@@ -0,0 +1,6 @@
+import setup
+
+
+if __name__ == "__main__":
+    for requirement in setup.REQUIREMENTS:
+        print requirement
--- setup.py
+++ setup.py
@@ -1,22 +1,25 @@
 from setuptools import setup
 
-setup(
-    name='dudebot',
-    version='0.0.4',
-    author='Sujay Mansingh',
-    author_email='sujay.mansingh@gmail.com',
-    packages=['dudebot', 'dudebot.examples'],
-    package_data={
-        'dudebot': ['README.md']
-    },
-    scripts=[],
-    url='https://github.com/sujaymansingh/dudebot',
-    license='LICENSE.txt',
-    description='A really simple framework for chatroom bots',
-    long_description=open('README.md').read(),
-    install_requires=[
-        'jabberbot==0.15',
-        'xmpppy==0.5.0rc1',
-        'feedparser==5.1.3',
-    ],
-)
+REQUIREMENTS = [
+    'jabberbot==0.15',
+    'xmpppy==0.5.0rc1',
+    'feedparser==5.1.3',
+]
+
+if __name__ == "__main__":
+    setup(
+        name='dudebot',
+        version='0.0.4',
+        author='Sujay Mansingh',
+        author_email='sujay.mansingh@gmail.com',
+        packages=['dudebot', 'dudebot.examples'],
+        package_data={
+            'dudebot': ['README.md']
+        },
+        scripts=[],
+        url='https://github.com/sujaymansingh/dudebot',
+        license='LICENSE.txt',
+        description='A really simple framework for chatroom bots',
+        long_description=open('README.md').read(),
+        install_requires=REQUIREMENTS
+    )
