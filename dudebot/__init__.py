7ad0e624e4bccab39b56152e9d4c6d5fba8dc528
Sujay Mansingh <sujay@editd.com>
2013-09-24 20:37:56 +0100

    Allow modules that use dudebot to just import dudebot...

---
+++ dudebot/__init__.py
@@ -0,0 +1,6 @@
+from core import BotAI
+from core import Connector
+
+from decorators import message_must_begin_with
+from decorators import message_must_begin_with_attr
+from decorators import message_must_begin_with_nickname
