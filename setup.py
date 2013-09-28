37ed551c618248e05d49993adc526277f6af0c28
Sujay Mansingh <sujay@editd.com>
2013-09-28 10:09:12 +0100

    Bump version number.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ REQUIREMENTS = [
 if __name__ == "__main__":
     setuptools.setup(
         name="dudebot",
-        version="0.0.6",
+        version="0.0.7",
         author="Sujay Mansingh",
         author_email="sujay.mansingh@gmail.com",
         packages=setuptools.find_packages(),
